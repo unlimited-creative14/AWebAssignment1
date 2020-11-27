fad7376444f69fadc3b73ad27d99623d2e63ed21
Le Huu Vo Long <long.leviathan14@hcmut.edu.vn>
2020-11-28 01:42:10 +0700

    First version of Home

--- js/home.js
+++ js/home.js
@@ -14,12 +14,47 @@ function createNavItem(listItem, name){
         document.getElementById("navListItem").append(li_tag)
     }
 }
+function createPartnerItem(partners){
+    for (let index = 0; index < partners.length; index++) {
+        const partner = partners[index];
+        const clientBox = document.getElementById("clientBox");
+
+        let pdiv = document.createElement("div");
+        let img = document.createElement("img");
+        let h = document.createElement("h4");
+        let desc = document.createElement("div");
+
+        pdiv.classList.add("col-sm-6", "col-lg-3", "text-center");
+
+        img.src = partner.src;
+        img.style.maxWidth = "100%";
+        img.alt = "logo" + partner.name;
+
+        h.innerText = partner.name;
+
+        pdiv.appendChild(img);
+        pdiv.appendChild(h);
+
+        clientBox.appendChild(pdiv);
+    }
+}
 items = [
-    {name : "Home",		link : "./index.html"},
-    {name : "About",	link : "./about.html"},
-    {name : "Works",	link : "./works.html"},
-    {name : "Services", link : "./services.html"},
-    {name : "Contacts", link : "./contacts.html"}                
+    {name : "Trang chủ",		link : "./index.html"},
+    {name : "Giới thiệu",	link : "./about.html"},
+    {name : "Dịch vụ", link : "./services.html"},
+    {name : "Bảng giá",	link : "./price.html"},
+    {name : "Liên hệ", link : "./contacts.html"}                
 ]
-createNavItem(items, name)
-window.scroll(0, 0)
+partners = [
+    {name: "Company 1", src: "./images/partner_icon/logo1.png", description: ""},
+    {name: "Company 2", src: "./images/partner_icon/logo2.png", description: ""},
+    {name: "Company 3", src: "./images/partner_icon/logo3.jpg", description: ""},
+    {name: "Company 4", src: "./images/partner_icon/logo4.jpg", description: ""},
+    {name: "Company 5", src: "./images/partner_icon/logo5.png", description: ""},
+    {name: "Company 6", src: "./images/partner_icon/logo2.png", description: ""},
+    {name: "Company 7", src: "./images/partner_icon/logo3.jpg", description: ""},
+    {name: "Company 8", src: "./images/partner_icon/logo4.jpg", description: ""}
+]
+createNavItem(items, name);
+window.scroll(0, 0);
+createPartnerItem(partners);
