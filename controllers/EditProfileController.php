8f46b1b37c0ad04f039c537162db4be9ac2f35b9
kAN <huukiethk2804@gmail.com>
2020-12-31 02:40:21 +0700

    update checker

--- controllers/EditProfileController.php
+++ controllers/EditProfileController.php
@@ -14,19 +14,19 @@ if (isset($_POST['address'])) {
 
 }
 
-// if (isset($_POST['Phone'])) {
-//     if ($_POST['Phone'] != "" && preg_match( '/^[0-9]+$/', $_POST['Phone'])) {
-//         echo '<div class="alert alert-danger">Số điện thoại không hợp lệ</div>';
-//         $isValid = false;
-//     } 
-// }
+if (isset($_POST['Phone'])) {
+    if ($_POST['Phone'] != "" && (preg_match('/^[0-9]$/', $_POST['Phone']) || strlen($_POST['Phone']) != 10)) {
+        echo '<div class="alert alert-danger">Số điện thoại không hợp lệ</div>';
+        $isValid = false;
+    } 
+}
 
-// if (isset($_POST["Email"])) {
-//     if ($_POST['Email'] != "" && !preg_match('/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i', $_POST["Email"])) {
-//         echo '<div class="alert alert-danger">Email không hợp lệ</div>';
-//         $isValid = false;
-//     }
-// }
+if (isset($_POST["Email"])) {
+    if ($_POST['Email'] != "" && !preg_match('/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i', $_POST["Email"])) {
+        echo '<div class="alert alert-danger">Email không hợp lệ</div>';
+        $isValid = false;
+    }
+}
 
 if ($isValid) {
     require("../models/EditProfileModel.php");
