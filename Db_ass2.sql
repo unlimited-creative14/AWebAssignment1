6a751c6e5509a984bb46d361d4e86cb756f19c3d
kAN <huukiethk2804@gmail.com>
2020-12-31 01:23:47 +0700

    update database

--- Db_ass2.sql
+++ Db_ass2.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Dec 30, 2020 at 01:21 PM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.4.10
+-- Host: 127.0.0.1
+-- Generation Time: Dec 30, 2020 at 07:23 PM
+-- Server version: 10.4.16-MariaDB
+-- PHP Version: 7.4.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `Db_ass2`
+-- Database: `db_ass2`
 --
 
 -- --------------------------------------------------------
@@ -29,26 +29,32 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `contact` (
   `contactID` int(11) NOT NULL,
-  `contactname` varchar(100) DEFAULT NULL,
-  `contacttype` varchar(100) DEFAULT NULL
+  `Fname` varchar(100) DEFAULT NULL,
+  `Lname` varchar(100) DEFAULT NULL,
+  `address` varchar(1000) DEFAULT NULL,
+  `Phone` varchar(10) DEFAULT NULL,
+  `Email` varchar(1000) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `contact`
 --
 
-INSERT INTO `contact` (`contactID`, `contactname`, `contacttype`) VALUES
-(1, NULL, NULL),
-(2, NULL, NULL),
-(3, NULL, NULL),
-(4, NULL, NULL),
-(6605240, NULL, NULL),
-(477464557, NULL, NULL),
-(533970266, NULL, NULL),
-(594477219, NULL, NULL),
-(781868515, NULL, NULL),
-(1013211327, NULL, NULL),
-(1621614655, NULL, NULL);
+INSERT INTO `contact` (`contactID`, `Fname`, `Lname`, `address`, `Phone`, `Email`) VALUES
+(1, NULL, NULL, '', '', ''),
+(2, NULL, NULL, '', '', ''),
+(3, NULL, NULL, '', '', ''),
+(4, NULL, NULL, '', '', ''),
+(376, 'Nguyễn Hữu', 'Kiệt', 'KTX Khu A', '0399822690', 'huukiethk2804@gmail.com'),
+(7311, 'Lamborgini', 'Nguyen ', 'Huu Kiet', '0123456789', 'huukiethk2804@gmail.com'),
+(8014, 'Nguyen ', 'Huu ', 'Kiet', '0123456789', 'Kiet.nguyenbk2804@hcmut.edu.vn'),
+(6605240, NULL, NULL, '', '', ''),
+(477464557, NULL, NULL, '', '', ''),
+(533970266, NULL, NULL, '', '', ''),
+(594477219, NULL, NULL, '', '', ''),
+(781868515, NULL, NULL, '', '', ''),
+(1013211327, NULL, NULL, '', '', ''),
+(1621614655, NULL, NULL, '', '', '');
 
 -- --------------------------------------------------------
 
@@ -72,11 +78,14 @@ CREATE TABLE `login` (
 --
 
 INSERT INTO `login` (`username`, `pass`, `contactID`, `birthday`, `create_time`, `lastaccess_time`, `avatar`, `role`) VALUES
+('HuuKiet', 'Kiet1234', 7311, NULL, NULL, NULL, NULL, 'user'),
+('huukiethk2804', 'Kiet1234', 376, NULL, NULL, NULL, NULL, 'user'),
 ('khanh', 'quockhanh1', 1, NULL, NULL, NULL, NULL, 'user'),
 ('khanh1', 'khanh123', 4, NULL, NULL, NULL, NULL, 'user'),
 ('khanh123', 'Khanh123', 477464557, NULL, NULL, NULL, NULL, 'user'),
 ('khanh1e2', 'Khanh123', 6605240, NULL, NULL, NULL, NULL, 'user'),
-('khanh@gmail.com', 'quockhanh', 3, NULL, NULL, NULL, NULL, 'user');
+('khanh@gmail.com', 'quockhanh', 3, NULL, NULL, NULL, NULL, 'user'),
+('Yassuo', 'Kiet1234', 8014, NULL, NULL, NULL, NULL, 'user');
 
 -- --------------------------------------------------------
 
@@ -135,10 +144,10 @@ INSERT INTO `testcontroller` (`username`, `pwd`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `UserTable`
+-- Table structure for table `usertable`
 --
 
-CREATE TABLE `UserTable` (
+CREATE TABLE `usertable` (
   `conntact_id` int(11) NOT NULL,
   `user_name` char(20) DEFAULT NULL,
   `pass` char(20) DEFAULT NULL,
@@ -166,9 +175,9 @@ ALTER TABLE `login`
   ADD KEY `contactID` (`contactID`);
 
 --
--- Indexes for table `UserTable`
+-- Indexes for table `usertable`
 --
-ALTER TABLE `UserTable`
+ALTER TABLE `usertable`
   ADD PRIMARY KEY (`conntact_id`);
 
 --
